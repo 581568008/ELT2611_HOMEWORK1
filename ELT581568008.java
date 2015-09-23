a78a4f0d58f8059c4395958bc6880170cc03f41e
581568008 <withun.sae@crru.ac.th>
2015-09-23 14:25:22 +0700

    ELT581568008
    ส่งงานครั้งที่ 1

---
+++ ELT581568008.java
@@ -0,0 +1,9 @@
+public class TestWhile {
+	public static void main (String[] args){
+		int count = 0;
+		while (count < 100){
+			Ssytem.out.println(count = ".welcom to Java class");
+			count++
+			}
+		}
+}
